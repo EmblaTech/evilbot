d5986300ec37864616fd403188e6bcd7a4f21fbd
Glitch (evilbot) <none>
2018-02-05 16:49:12 +0000

    🛢🌽 Checkpoint bot.js:288663/188 bot.bup.js:288663/5

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const evil = require('./evil/startup').ref;
 
 const send_text = require('./evil/send_text').ref;
+const send_attachment = require('./evil/send_attachment').ref;
 
 // evil({
 //   flow: {
@@ -14,6 +15,9 @@ evil({
   flow: {
     on_message: (x) => {
       send_text(x.user_id, 'Hello!!!');
+    },
+    on_attachment: (x) => {
+      send_attachment(x.user_id, 'image', {url: 'https://cdn.glitch.com/5655c833-6ba1-4cae-a038-c785bce441e8%2Fsixteen.png?1517748298720', is_reusable: true},);
     }
   }
 })
