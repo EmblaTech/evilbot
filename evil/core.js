184aff055450bdf6ae1eb4463685871a0cea674e
Glitch (evilbot) <none>
2018-02-13 16:04:58 +0000

    🍑🚃 Checkpoint evil/__handle_message.js:288663/9 evil/core.js:288663/503

--- evil/core.js
+++ evil/core.js
@@ -3,8 +3,21 @@ const startup = require('./startup').ref
 module.exports = (_) => {
   const configs = _.configs || {}
   
+  const flowSubscribers = {
+    on_get_started: [],
+    on_postback: []
+  }
+  const bindSubscribers = (idx) => {
+    return (_) => flowSubscribers[idx].forEach(subs => subs(_))
+  }
+  const flow = {
+    on_get_started: (_) => flowSubscribers.on_get_started.forEach(subs => subs(_)),
+    on_postback: bindSubscribers('on_postback')
+  }
+  
   startup({
-    configs: configs
+    configs: configs,
+    flow: flow
   })
   
   const services = _.services || []
@@ -27,9 +40,8 @@ module.exports = (_) => {
   
   interactions.forEach(_ => _.inject({
     listenTo: {
-      getStarted: (f) => [].push(f),
+      getStarted: (f) => flow.on_get_started.push(f),
       text: (f) => [].push(f),
-      quickReply: (f) => [].push(f),
       postback: (f) => [].push(f),
       attachment: (f) => [].push(f),
       other: (f) => [].push(f)
