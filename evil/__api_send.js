2477499bb2a41029ae7b842cba402464c68fc02f
Glitch (evilbot) <none>
2018-02-13 17:54:55 +0000

    💒👏 Checkpoint evil/send_text.js:288663/8 evil/__api_send.js:288663/403

--- evil/__api_send.js
+++ evil/__api_send.js
@@ -1,6 +1,10 @@
 const request = require('request');
 
 var sendMessage = (messageData, on_success, on_error) => {
+  
+  const subscribers = []
+  const errorHandlers = []
+  
   request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
     qs: { access_token: process.env.PAGE_ACCESS_TOKEN, method: 'POST' },
@@ -13,19 +17,28 @@ var sendMessage = (messageData, on_success, on_error) => {
       var recipientId = body.recipient_id;
       var messageId = body.message_id;
       
-      if (on_success) on_success(body, response);
-      
       console.log("Successfully SENT generic message with id %s to recipient %s", messageId, recipientId);
       
+      if (on_success) on_success(body, response);
+      
+      subscribers.forEach(_ => _(body,response))
+      subscribers = null;
+      
     } else {
-      
-      if (on_error) on_error(error, body, response);
-      
       console.error("Unable to send message.");
       console.error(error, response.statusCode);
+      
+      if (on_error) on_error(error, body, response);
     
+      errorHandlers.forEach(_ => _(error, response.statusCode))
+      errorHandlers = null
     }
-  });  
+  });
+  
+  return {
+    subscribe: (_) => subscribers.push(_),
+    error: (_) => errorHandlers.push(_)
+  }
 }
 
 exports.ref = sendMessage;
